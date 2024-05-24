2d741e843de00037c87f04aa994bd4a2c58a2ebf
Murodbek0 <tursunaliyevmurodbek07@gmail.com>
2024-05-24 06:53:39 +0500

    issues

---
+++ js/index.js
@@ -0,0 +1,45 @@
+// 1 misol
+
+// let t = +prompt ("ketish vaqti");
+// let T = +prompt ("kechikan vaqti")
+
+// let  vaqti =(t+T)%24;
+// console.log(vaqti);
+
+
+
+
+//    2 misol
+// alert("haroratni s da krting");
+// let s = +prompt('s ni krting');
+
+
+// let k =273;
+// let f =1.80+32;
+
+
+// let kelvin;
+// let Fahrenheit;
+
+
+// kelvin = k+s;
+// Fahrenheit = f+s;
+
+// console.log(kelvin);
+// console.log(Fahrenheit);
+
+
+// 3 misol
+
+// alert("Tomonlari soni N ga teng bo’lgan qavariq ko’pburchakning ichki burchaklari yig’indisini toping.");
+// let N = +prompt('N sonni krting');
+ 
+// let = + ((N - 2) * 180)
+
+// console.log('');
+
+
+
+// 4 misol
+
+
